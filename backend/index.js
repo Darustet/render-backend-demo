ff2137820ba84e7279cbd460ae567321e3f49b06
Darustet <58421506+Darustet@users.noreply.github.com>
2025-09-24 11:40:48 +0300

    Added mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const Note = require('./models/note')
 
+const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 
@@ -26,8 +28,11 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World! Are you listening?</h1>')
 })
 
+
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -82,7 +87,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
