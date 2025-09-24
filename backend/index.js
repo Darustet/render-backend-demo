8889987d900d815149e36554fab78103f41e302f
Darustet <58421506+Darustet@users.noreply.github.com>
2025-09-24 13:25:39 +0300

    Updated notes backend to use mongoDB.

--- backend/index.js
+++ backend/index.js
@@ -36,13 +36,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-    if (note) {    
-        response.json(note)  
-    } else {    
-        response.status(404).end()  
-    }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -68,16 +64,14 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
   
-  notes = notes.concat(note)
-
-  console.log(note)
-  response.json(note)
+  note.save().then(savedNote => { 
+    response.json(savedNote)
+  })
 })
 
 
